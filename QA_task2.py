921738854d41804ff39381f4eeaf563d67d6c363
Natalia_Perchishena <chilly233@gmail.com>
2021-04-25 21:32:28 +0300

    adding function calc

---
+++ QA_task2.py
@@ -0,0 +1,79 @@
+#function calc
+def calculate():
+    number_1 = int(input('Please enter the first number: '))
+    number_2 = int(input('Please enter the second number: '))
+    operation = input('''
+        Please type in the math operation you would like to complete:
+         + for addition
+         - for subtraction
+         * for multiplication
+        / for division
+    ''')
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
+    else:
+        print('You have not typed a valid operator, please run the program again.')
+    # Добавление функции again() в calculate()
+    again()
+
+
+def again():
+   calc_again = input('''
+     Do you want to calculate again?
+     Please type Y for YES or N for NO.
+     ''')
+
+
+   if calc_again.upper() == 'Y':
+    calculate()
+   elif calc_again.upper() == 'N':
+    print('See you later.')
+   else:
+    again()
+calculate()
+
+
+#function simple calc
+def mycalc():
+    switcher = {
+        "-": (a - b),
+        "+": (a + b),
+        '*': (a * b),
+        '/': (a / b)
+    }
+    print(a, operator, b, '=', +switcher[operator])
+
+
+a = int(input('input a\n'))
+b = int(input('input b\n'))
+operator = input('input operator +,-,*, /\n')
+mycalc()
+
+
+#function simple calc2
+def simplecalc(a, b, c):
+    if c == '+':
+        return a+b
+    elif c == '-':
+        return a-b
+    elif c == '*':
+        return a*b
+    elif c =='/':
+        return a/b
+    else:
+       return ('nonetype operator')
+
+a = int(input('input a\n'))
+b = int(input('input b\n'))
+c = input('input c (operator +,-,*, /)\n')
+print(a, c, b, '=', str(simplecalc(a, b, c)))
